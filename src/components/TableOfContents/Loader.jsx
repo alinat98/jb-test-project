8dd40c894dcefa125583e4f0ef5309a05a6af31e
DESKTOP-EKN07JF\alina <alina.natrobina@simbirsoft.com>
2021-07-02 22:35:21 +0300

    add loader

---
+++ src/components/TableOfContents/Loader.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const Loader = () => (
+  <Wrapper>
+    {[...Array(10)].map(() => (
+      <LoaderItem />
+    ))}
+  </Wrapper>
+);
+
+export default Loader;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const LoaderItem = styled.li`
+  height: 16px;
+  margin: 8px 32px;
+  background: #0000000c;
+  :nth-child(2),
+  :nth-child(3),
+  :nth-child(4) {
+    margin-left: 48px;
+  }
+  :nth-child(5),
+  :nth-child(6),
+  :nth-child(7),
+  :nth-child(8) {
+    margin-left: 64px;
+  }
+  :nth-child(2n):not(:last-child) {
+    margin-right: 64px;
+  }
+`;
--- src/components/TableOfContents/index.jsx
+++ src/components/TableOfContents/index.jsx
@@ -1,28 +1,36 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import List from "./List";
+import Loader from "./Loader";
 
 const TableOfContents = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState({});
   const [selected, setSelected] = useState("");
 
   useEffect(() => {
+    setIsLoading(true);
     fetch("http://localhost:8000")
       .then((response) => response.json())
       .then((data) => setData(data))
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
   }, []);
 
   return (
     <Wrapper>
-      {data.topLevelIds && (
-        <List
-          listOfKeys={data.topLevelIds}
-          dataCollection={data.entities.pages}
-          data={data.entities}
-          selected={selected}
-          setSelected={setSelected}
-        />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        data.topLevelIds && (
+          <List
+            listOfKeys={data.topLevelIds}
+            dataCollection={data.entities.pages}
+            data={data.entities}
+            selected={selected}
+            setSelected={setSelected}
+          />
+        )
       )}
     </Wrapper>
   );
